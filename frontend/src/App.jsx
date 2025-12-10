abbffb084c61c04f1361b86d77235d4e108aa69b
Justin Truong <justintruong222@gmail.com>
2025-12-10 05:11:31 -0600

    minor styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [books, setBooks]=useState([]);
-  const [search,setSearch]=useState('');
-  const [loading,setLoading]=useState(false);
-  const [error, setError]=useState('');
-  const searchBooks= async(event)=>{
-    event.preventDefault()
+  const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const searchBooks = async (event) => {
+    event.preventDefault();
     try {
-      setError('');
+      setError("");
       const response = await fetch(`http://localhost:3000/search?q=${search}`);
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       setBooks(data);
     } catch (err) {
       console.error("Search failed:", err);
@@ -27,26 +27,37 @@ function App() {
     } finally {
       setLoading(false);
     }
-
-  }
-
+  };
 
   return (
-    <div>
-      <form onSubmit={searchBooks}>
-        <input placeholder='search ISBN, Tile, Authors, Status...' value={search} onChange={(e)=>setSearch(e.target.value)}/>
-        <button>search</button>
-      </form>
-      {error&&<p>{error}</p>}
+    <div className="homebox-1">
+      <div className="homebox-2">
+        <form onSubmit={searchBooks}>
+          <input
+            className="search-input-field"
+            placeholder="search ISBN, Tile, Authors, Status..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button>search</button>
+        </form>
+        {error && <p>{error}</p>}
 
-      {loading?<p>loading...</p>:<li>
-        {books.map((book)=>(<ol key={book.ISBN}>
-          ISBN:{book.ISBN}, Title:{book.Title}, Authors:{book.Authors}, Status:{book.Status}
-        </ol>))}
-      </li>}
-
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <li className="no-bullet-point">
+            {books.map((book) => (
+              <ol key={book.ISBN}>
+                ISBN:{book.ISBN}, Title:{book.Title}, Authors:{book.Authors},
+                Status:{book.Status}
+              </ol>
+            ))}
+          </li>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
